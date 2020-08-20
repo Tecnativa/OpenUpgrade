ac0334610b325aa81e4c1c5d89daa0e608e2b524
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-21 01:25:17 +0200

    [ADD] rma + website_product_attribute_image

--- addons/product/migrations/12.0.1.2/post-migration.py
+++ addons/product/migrations/12.0.1.2/post-migration.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openupgradelib import openupgrade
+from openupgradelib import openupgrade, openupgrade_90
 from psycopg2.extensions import AsIs
 
 
@@ -21,8 +21,38 @@ def map_product_attribute_create_variant(cr):
         """, (AsIs(openupgrade.get_legacy_name('create_variant')), ),
     )
 
+def _migrate_website_product_attribute_image(env):
+    openupgrade.update_module_names(
+        env.cr, [(
+            "website_product_attribute_image",
+            "website_sale_product_detail_attribute_value_image",
+        )], merge_modules=True,
+    )
+    openupgrade.rename_columns(
+        env.cr, {"product_attribute_value": [("attr_image", None)]}
+    )
+    openupgrade.rename_fields(
+        env,
+        [
+            (
+                "product.attribute.value",
+                "product_attribute_value",
+                "attr_image",
+                "website_product_detail_image",
+            )
+        ],
+    )
+    openupgrade_90.convert_binary_field_to_attachment(
+        env,
+        {"product.attribute.value": [
+            ("website_product_detail_image", 
+             openupgrade.get_legacy_name("attr_image")),
+        ]},
+    )
+
 
 @openupgrade.migrate()
 def migrate(env, version):
     env['product.category']._parent_store_compute()
     map_product_attribute_create_variant(env.cr)
+    _migrate_website_product_attribute_image(env)
--- odoo/addons/base/migrations/12.0.1.3/pre-migration.py
+++ odoo/addons/base/migrations/12.0.1.3/pre-migration.py
@@ -170,6 +170,185 @@ def fill_ir_attachment_res_model_name(cr):
         )
 
 
+def _migrate_rma(env):
+    cr = env.cr
+    # Migration scripts to be able to use in v12 'rma' and 'rma_sale'
+    # modules from OCA/rma instead of the existing private modules
+    # rma_ept and website_rma_ept.
+    openupgrade.update_module_names(
+        cr, [("rma_ept", "rma"), ("website_rma_ept", "rma_sale")], merge_modules=True,
+    )
+    openupgrade.rename_models(cr, [("rma.reason.ept", "rma.operation")])
+    openupgrade.rename_tables(cr, [("rma_reason_ept", "rma_operation")])
+    # set 'claim.line.ept' as the main rma table renaming it to 'rma'
+    openupgrade.rename_models(cr, [("claim.line.ept", "rma")])
+    openupgrade.rename_tables(cr, [("claim_line_ept", "rma")])
+    # Remove incorrect lines
+    openupgrade.logged_query(cr, "DELETE FROM rma WHERE claim_id IS NULL")
+    # Rename security groups
+    openupgrade.rename_xmlids(
+        env.cr, [
+            ("rma_ept.group_rma_user_ept", "rma.rma_group_user_all"),
+            ("rma_ept.group_rma_manager_ept", "rma.rma_group_manager"),
+        ]
+    )
+    # Rename existing fields in the new rma table to be compatible
+    # with rma v12 module
+    openupgrade.rename_fields(
+        env,
+        [
+            ("rma", "rma", "quantity", "product_uom_qty"),
+            ("rma", "rma", "rma_reason_id", "operation_id"),
+        ],
+    )
+    # Add non-existing field in the new rma table to be filled
+    openupgrade.add_fields(
+        env,
+        [
+            ("sent", "rma", "rma", "boolean", False, "rma"),
+            ("name", "rma", "rma", "char", False, "rma"),
+            ("origin", "rma", "rma", "char", False, "rma"),
+            ("date", "rma", "rma", "datetime", False, "rma"),
+            ("deadline", "rma", "rma", "date", False, "rma"),
+            ("user_id", "rma", "rma", "many2one", False, "rma"),
+            ("company_id", "rma", "rma", "many2one", False, "rma"),
+            ("partner_id", "rma", "rma", "many2one", False, "rma"),
+            ("partner_invoice_id", "rma", "rma", "many2one", False, "rma"),
+            ("picking_id", "rma", "rma", "many2one", False, "rma"),
+            ("product_uom", "rma", "rma", "many2one", False, "rma"),
+            ("priority", "rma", "rma", "selection", False, "rma"),
+            ("state", "rma", "rma", "selection", False, "rma"),
+            ("description", "rma", "rma", "text", False, "rma"),
+            ("location_id", "rma", "rma", "many2one", False, "rma"),
+            ("reception_move_id", "rma", "rma", "many2one", False, "rma"),
+            ("refund_id", "rma", "rma", "many2one", False, "rma"),
+            ("refund_line_id", "rma", "rma", "many2one", False, "rma"),
+            # rma_sale module
+            ("order_id", "rma", "rma", "many2one", False, "rma_sale"),
+        ],
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE rma AS r
+        SET description = cce.name || E'\n' || cce.description || E'\n'
+                || cce.cause || E'\n' || cce.resolution,
+            sent = cce.rma_send,
+            name = cce.code,
+            date = cce.date,
+            deadline = cce.date_deadline,
+            user_id = cce.user_id,
+            company_id = cce.company_id,
+            partner_id = cce.partner_id,
+            picking_id = cce.picking_id,
+            priority = cce.priority,
+            location_id = cce.location_id,
+            order_id = so.id,
+            origin = so.name,
+            partner_invoice_id = so.partner_invoice_id,
+            product_uom = sm.product_uom,
+            state = (
+                CASE
+                    WHEN cce.state = 'approve' THEN 'confirmed'
+                    WHEN cce.state = 'process' THEN 'received'
+                    WHEN cce.state = 'reject' THEN 'cancelled'
+                    WHEN (cce.state = 'close' AND r.claim_type = 'refund')
+                        THEN 'refunded'
+                    WHEN (cce.state = 'close' AND r.claim_type = 'replace')
+                        THEN 'replaced'
+                    WHEN (cce.state = 'close' AND r.claim_type = 'repair')
+                        THEN 'returned'
+                END
+            )
+        FROM
+            crm_claim_ept AS cce,
+            stock_picking AS sp,
+            sale_order AS so,
+            stock_move AS sm
+        WHERE
+            r.claim_id = cce.id
+            AND cce.picking_id = sp.id
+            AND sp.sale_id = so.id
+            AND r.move_id = sm.id
+        """,
+    )
+    # Find matching refund and refund line
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE rma AS r
+        SET
+            refund_line_id = ail.id,
+            refund_id = ai_cce.account_invoice_id
+        FROM
+            crm_claim_ept AS cce,
+            account_invoice_crm_claim_ept_rel AS ai_cce,
+            account_invoice_line AS ail
+        WHERE
+            r.claim_id = cce.id
+            AND ai_cce.crm_claim_ept_id = cce.id
+            AND ai_cce.account_invoice_id = ail.invoice_id
+            AND r.product_id = ail.product_id
+        """,
+    )
+    # Find matching reception_move_id
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE rma AS r
+        SET reception_move_id = sm.id
+        FROM
+            stock_move AS sm,
+            crm_claim_ept AS cce
+        WHERE
+            r.claim_id = cce.id
+            AND cce.return_picking_id = sm.picking_id
+            AND r.product_id = sm.product_id
+        """,
+    )
+    # Set rma_id in account.invoice.line
+    openupgrade.add_fields(
+        env,
+        [
+            (
+                "rma_id",
+                "account.invoice.line",
+                "account_invoice_line",
+                "many2one",
+                False,
+                "rma",
+            )
+        ],
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_invoice_line AS ail
+        SET rma_id = r.id
+        FROM rma AS r
+        WHERE r.refund_line_id = ail.id
+        """,
+    )
+    # Set rma_id in stock.picking
+    openupgrade.add_fields(
+        env, [("rma_id", "stock.move", "stock_move", "many2one", False, "rma")],
+    )
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE stock_move AS sm
+        SET rma_id = r.id
+        FROM
+            crm_claim_ept_stock_picking_rel AS cce_sp,
+            rma AS r
+        WHERE
+            sm.picking_id = cce_sp.stock_picking_id
+            AND r.claim_id = cce_sp.crm_claim_ept_id
+            AND r.product_id = sm.product_id
+        """,
+    )
+
+
 @openupgrade.migrate(use_env=True)
 def migrate(env, version):
     openupgrade.remove_tables_fks(env.cr, _obsolete_tables)
@@ -234,3 +413,4 @@ def migrate(env, version):
             'view_menu',
             'lang_km',
         ], False)
+    _migrate_rma(env)
